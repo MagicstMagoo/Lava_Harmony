f4546c1c70236e0e797672a535c8bae6b6909109
麻瓜瓜 <96295450+MagicstMagoo@users.noreply.github.com>
2022-07-07 09:03:26 +0800

    基础文件

---
+++ entry/src/main/java/cn/magicst/lava/harmony/MainAbility.java
@@ -0,0 +1,15 @@
+package cn.magicst.lava.harmony;
+
+import cn.magicst.lava.harmony.slice.MainAbilitySlice;
+//注释
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+
+    }
+}
---
+++ entry/src/test/java/cn/magicst/lava/harmony/ExampleTest.java
@@ -0,0 +1,9 @@
+package cn.magicst.lava.harmony;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
